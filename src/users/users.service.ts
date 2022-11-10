1c954133c5145aaaed0d1d0e355698b4bc4bc6c4
Abderraouf Tchekioun <Abdelraouffinch@gmail.com>
2022-11-10 17:22:55 +0100

    Omit the password and valdiate the user

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,14 +1,15 @@
 import { Injectable } from '@nestjs/common';
-import { Prisma } from '@prisma/client';
+import { Prisma, User } from '@prisma/client';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { NotFoundException } from '@nestjs/common/exceptions';
 
 @Injectable()
 export class UsersService {
-  constructor(private readonly prisma:PrismaService){}
+  constructor(private readonly prisma: PrismaService) {}
 
-  async create(data: Prisma.UserCreateInput) {
-    const {password, ...rest} = await this.prisma.user.create({data});
+  async create(data: Prisma.UserCreateInput): Promise<Omit<User, 'password'>> {
+    const { password, ...rest } = await this.prisma.user.create({ data });
     return rest;
   }
 
@@ -17,8 +18,16 @@ export class UsersService {
   }
 
   findOne(id: number) {
+    return this.prisma.user.findUnique({ where: { id } });
+  }
 
-    return this.prisma.user.findUnique({where:{id}});
+  async findByUsername(username: string): Promise<User | null> {
+    const user = await this.prisma.user.findUnique({ where: { username } });
+    if (!user) {
+      throw new NotFoundException();
+    }
+
+    return user;
   }
 
   update(id: number, updateUserDto: UpdateUserDto) {
