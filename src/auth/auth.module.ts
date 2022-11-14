87df0a393abe1ac17d72778f8eff1c1cd5c7ae2f
Abderraouf Tchekioun <Abdelraouffinch@gmail.com>
2022-11-14 16:19:42 +0100

    can login, but still can't verify the logged in users

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
+import { PassportModule } from '@nestjs/passport';
 import { UsersModule } from 'src/users/users.module';
 import { AuthService } from './auth.service';
+import { LocalStrategy } from './local.strategy';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
-  imports: [UsersModule],
-  providers: [AuthService],
+  imports: [
+    UsersModule,
+    PassportModule,
+    JwtModule.register({
+      secret: 'shouldBeFromEnv',
+      signOptions: { expiresIn: '120s' },
+    }),
+  ],
+  providers: [AuthService, LocalStrategy],
+  exports: [AuthService],
 })
 export class AuthModule {}
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,9 +1,13 @@
 import { Injectable } from '@nestjs/common';
 import { UsersService } from 'src/users/users.service';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
-  constructor(private usersService: UsersService) {}
+  constructor(
+    private usersService: UsersService,
+    private jwtService: JwtService,
+  ) {}
 
   async validateUser(username: string, password: string): Promise<any> {
     const user = await this.usersService.findByUsername(username);
@@ -12,5 +16,16 @@ export class AuthService {
       return rest;
     }
     return null;
+  }
+
+  async login(user: any) {
+    const payload = {
+      sub: user.id,
+      username: user.username,
+      fullnam: user.fullnam,
+    };
+    return {
+      access_token: this.jwtService.sign(payload),
+    };
   }
 }
