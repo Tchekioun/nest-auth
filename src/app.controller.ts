90b4dcbba4208e3e83c34ac293806d0825691287
Abderraouf <abdelraouffinch@gmail.com>
2023-02-14 23:14:21 +0100

    change the return object

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -12,6 +12,7 @@ export class AppController {
   @UseGuards(LocalAuthGuard)
   @Post('login')
   async login(@Request() req) {
-    return this.authService.login(req.user);
+    const access_token = await this.authService.login(req.user);
+    return { ...access_token, user: req.user };
   }
 }
